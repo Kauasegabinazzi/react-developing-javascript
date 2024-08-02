11642c8a220c074d27d0f651d9b7ae96781681cf
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-02 01:04:40 -0300

    1-commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editdsds <code>src/App.js</code> and save to reloadsad.
         </p>
         <a
           className="App-link"
