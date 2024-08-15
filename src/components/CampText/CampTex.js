64a4a308895ae712784d12eb67cb358aa9e9bc92
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-15 00:02:57 -0300

    12-commit

--- src/components/CampText/CampTex.js
+++ src/components/CampText/CampTex.js
@@ -3,10 +3,16 @@ import './CampText.css';
 const CampText = (props) => {
     const placeholderModificada = `${props.placeholder}...`
 
+    let value = '';
+
+    const aoDigitado = (evento) =>{
+        value = evento.target.value;
+    }
+
     return (
         <div className="campo-text">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}></input>
+            <input value={value} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}></input>
         </div>
     )
 }
