db25914489ecd26e377e58bfd64c26924c77d965
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-08-16 22:56:57 -0300

    15-commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import Banner from './components/Banner/index';
 import Formulario from './components/formulario/index';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario></Formulario>
+      <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado}></Formulario>
     </div>
     
   );
--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -4,7 +4,7 @@ import List from '../List/index';
 import Buttons from '../Button/index';
 import { useState } from 'react';
 
-const Formulario = () => {
+const Formulario = (props) => {
 
     const times = [
         'Programação',
@@ -23,7 +23,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault();
-        console.log(time);
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (
